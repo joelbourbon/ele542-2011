89edb76cd2dd4126c0a4aa50866468c55f9e14b4
olivierlmass1@gmail.com <olivierlmass1@gmail.com@f18b34fc-fa4a-9b25-d40f-964363eed865>
2012-03-02 21:08:01 +0000

    ADC all setuped not tested

--- trunk/ELE_542_Projet/core/ADC/adc.cpp
+++ trunk/ELE_542_Projet/core/ADC/adc.cpp
@@ -13,26 +13,52 @@
 #include "adc.h"
 
 
-adc::adc()
+adc::adc():
+  first(1)
 {
 	
 	ADMUX = (0 << REFS1)      // 
 	      | (0 << REFS0)      // Reference selection Bits: AREF, internal Vref turned off
-        | (0 << ADLAR )     //
-		    | (0 << MUX4 )      //
-		    | (0 << MUX3 )      //
-		    | (0 << MUX2 )      //
+        | (0 << ADLAR)     //
+		    | (0 << MUX4)      //
+		    | (0 << MUX3)      //
+		    | (0 << MUX2)      //
 		    | (0 << MUX1)       //  Analog Channel and gain selection bits
-		    | (1 << MUX0);      // 
+		    | (0 << MUX0);      // 
   
-	ADCSRA = (1 << ADEN)      // ADC Enable
-		    | (1 << ADSC)       // ADC Start Conversion
-		    | (0 << ADATE)      // Auto trigger Disabled
-		    | (1 << ADIE );     // ADC Interrupt Enabled 
-  
-	
   SFIOR = (0 << ADTS2)      // Free Running mode
 		    | (0 << ADTS1)      // 
 		    | (0 << ADTS0);     // 
+			
+	ADCSRA = (1 << ADEN)      // ADC Enable
+		     | (1 << ADSC)       // ADC Start Conversion
+		     | (0 << ADATE)      // Auto trigger Disabled
+		     | (1 << ADIE);     // ADC Interrupt Enabled 
+  
+}
 
+ISR(ADC_vect){
+	
+	if(1==s.ADC1.first){
+		s.ADC1.first=0;
+		ADMUX^=(1<<0);
+	}
+	
+  else
+  {
+	  s.ADC1.adc1.adc[0]=ADCL;
+	  s.ADC1.adc1.adc[1]=ADCH;
+	  
+    if((ADMUX & MUXMASK) == 0){
+      ADMUX&=~(1<<0);  
+	    s.ADC1.sum_motor_left+=s.ADC1.adc1.adc_value;
+		  s.ADC1.nb_measure_left++;
+    }
+  	 
+    else{
+      ADMUX|=(1<<0);  
+	    s.ADC1.sum_motor_right+=s.ADC1.adc1.adc_value;
+      s.ADC1.nb_measure_right++;
+    }
+  }
 }
--- trunk/ELE_542_Projet/init/initcore.h
+++ trunk/ELE_542_Projet/init/initcore.h
@@ -18,6 +18,7 @@
 #include "core/UART/uart.h"
 #include "core/Timer/timer0.h"
 #include "core/Timer/timer1.h"
+#include "core/ADC/adc.h"
 #include "core/Controle/moteur.h"
 #include "core/datastore.h"
 //#include "core/PIN/pin.h"
@@ -34,6 +35,7 @@ public:
 	timer1 Timer1;
 	moteur Moteur;
 	datastore DataStore;
+	adc ADC1;
 	
 	//pin DIR_G1;
 	//pin DIR_G2;
