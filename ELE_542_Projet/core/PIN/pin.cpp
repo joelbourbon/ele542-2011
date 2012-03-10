1947747537a78dd20e2c36a1898fffd21cc9d569
joelbourbon@gmail.com <joelbourbon@gmail.com@f18b34fc-fa4a-9b25-d40f-964363eed865>
2012-03-10 21:19:57 +0000

    Avancement, petites corrections mais PWM toujours non fonctionnel

--- ELE_542_Projet/core/PIN/pin.cpp
+++ ELE_542_Projet/core/PIN/pin.cpp
@@ -14,10 +14,11 @@ pin::pin(volatile uint8_t *iPORTX, volatile uint8_t *iDDRX, volatile uint8_t *iP
   *DDRX = (*DDRX & ~(1 << PINNumber))    // Ensure a 0 in the pin position
         | (iPinDirection << PINNumber);  // Put the good value to set direction
 		
+	// Output --> Set (1)         Input --> Clear (0)
 	if(iPinDirection == PinOutput)
-	  *PORTX &= ~(1 << PINNumber);
-	else
 	  *PORTX |=  (1 << PINNumber);
+	else
+	  *PORTX &= ~(1 << PINNumber);
 }
 
 pin::pin(volatile uint8_t *iPORTX, volatile uint8_t *iDDRX, uint8_t iPinNumber, PINDirection iPinDirection)
